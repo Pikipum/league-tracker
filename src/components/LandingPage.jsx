37c7582980a4ac9e924d8738a37b3a6b4caa9594
Markus Tervonen <markus.tervonen@hotmail.com>
2026-01-29 15:06:59 +0200

    removed unused import

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import FavoriteProfiles from "./FavoriteProfiles";
-import LogInButton from "./LogInButton";
 import SearchBar from "./SearchBar";
 import NavigationBar from "./NavigationBar";
 
